17d9325dee6fa4f77b3e1a86020780df8db01c24
Rinooj NR <rinoojnr@gmail.com>
2021-05-11 20:40:02 +0530

    added sorting_arithematic_computation_program.sh

--- sorting_arithematic_computation_program.sh
+++ sorting_arithematic_computation_program.sh
@@ -17,3 +17,10 @@ declare -A results
 results=( ["result1"]=$result1 ["result2"]=$result2 ["result3"]=$result3 ["result"]=$result )
 
 echo ${results[@]}
+
+array[0]=${results["result1"]}
+array[1]=${results["result2"]}
+array[2]=${results["result3"]}
+array[3]=${results["result"]}
+
+echo ${array[@]}
